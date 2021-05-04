16734ea991c77d3d0bb2f33bd26b87d6cd95390c
Steffan Norberhuis <steffan@norberhuis.nl>
2021-05-04 14:08:29 +0200

    Add PUT and GET proxy+ integration.

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+## Source
+
+Based upon:
+1. https://github.com/msysh/aws-sample-cdk-apigateway-proxy-s3
+1. https://github.com/amitevski/apigatewaystaticsite/blob/c84fddcc212eead2de22321cd89d4bda0155eb64/cdk/lib/cdk-stack.ts
+
--- lib/cdk-apigateway-proxy-s3-stack.ts
+++ lib/cdk-apigateway-proxy-s3-stack.ts
@@ -4,99 +4,103 @@ import * as s3 from '@aws-cdk/aws-s3';
 import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class CdkApigatewayProxyS3Stack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        //
+        // S3 bucket
+        //
+        const bucket = new s3.Bucket(this, `bucket`, {
+            encryption: s3.BucketEncryption.KMS_MANAGED,
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
 
-    // The code that defines your stack goes here
-    const PREFIX :string = this.node.tryGetContext('prefix');
+        const methodResponses =[
+            {
+                statusCode: '200',
+                responseParameters: {
+                    'method.response.header.Content-Type': false,
+                    'method.response.header.Content-Disposition': false,
+                }
+            },
+            {statusCode: '404'},
+        ];
 
-    //
-    // S3 bucket
-    //
-    const bucket = new s3.Bucket(this, `${PREFIX}bucket`, {
-      encryption: s3.BucketEncryption.KMS_MANAGED,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      bucketName: `${PREFIX}bucket`,
-      removalPolicy: cdk.RemovalPolicy.DESTROY
-    });
+        const integrationResponses = [
+            {
+                statusCode: '200',
+                responseParameters: {
+                    'method.response.header.Content-Type': 'integration.response.header.Content-Type',
+                    'method.response.header.Content-Disposition': 'integration.response.header.Content-Disposition',
+                },
+            },
+            {selectionPattern: '^404', statusCode: '404'},
+        ];
 
-    //
-    // IAM Role
-    //
-    const policyStatement = new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-    })
-    policyStatement.addActions(
-      's3:PutObject'
-    );
-    policyStatement.addResources(
-      bucket.bucketArn + '/*'
-    );
+        const role = new iam.Role(this, 'S3IntegrationRole', {
+            assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+        });
+        bucket.grantRead(role);
+        bucket.grantPut(role)
 
-    const policyDocument = new iam.PolicyDocument({statements:[policyStatement]})
+        const api = new apigateway.RestApi(this, 'api', {
+            deployOptions: {
+                metricsEnabled: true,
+                loggingLevel: apigateway.MethodLoggingLevel.INFO,
+            },
+            binaryMediaTypes: [
+                // To stop API GW from touching binary files
+                'image/*',
+            ],
+        });
+        // Hack to disable remote api endpoint
+        // (api.node.children[0] as apigateway.CfnRestApi).addPropertyOverride('DisableExecuteApiEndpoint','true')
 
-    const role = new iam.Role(this, `${PREFIX}role`, {
-      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
-      description: 'CDK for API Gateway Proxy for S3',
-      roleName: `${PREFIX}role`,
-      inlinePolicies: {
-        'policy': policyDocument
-      }
-    });
 
-    //
-    // API Gateway
-    //
-    const api = new apigateway.RestApi(this, `${PREFIX}apigateway`,{
-      restApiName: `${PREFIX}apigw`,
-      endpointTypes: [apigateway.EndpointType.REGIONAL],
-      description: 'CDK for API Gateway Proxy for S3'
-    });
-    const bucketNameResource = api.root.addResource('{bucket_name}');
-    const objectKeyResource = bucketNameResource.addResource('{object_key}')
-    objectKeyResource.addMethod('PUT', new apigateway.AwsIntegration({
-      service: 's3',
-      integrationHttpMethod: 'PUT',
-      path: '{bucket_name}/{object_key}',
-      // @aws-cdk/aws-apigateway.IntegrationOptions
-      options: {
-        credentialsRole: role,
-        requestParameters: {
-          "integration.request.path.bucket_name": "method.request.path.bucket_name",
-          "integration.request.path.object_key": "method.request.path.object_key"
-        },
-        integrationResponses: [
-          {
-            statusCode: "200",
-            // contentHandling: pass-thru
-          }
-        ]
-      }
-    }),
-    // @aws-cdk/aws-apigateway.MethodOptions
-    {
-      requestParameters: {
-        "method.request.path.bucket_name": true,
-        "method.request.path.object_key": true
-      },
-      methodResponses: [
-        {
-          statusCode: "200",
-          responseModels: {
-            "application/json": new apigateway.EmptyModel()
-          }
-        }
-      ]
-    });
+        api.root.addMethod('GET', undefined, {
+            methodResponses,
+        });
 
-    //
-    // Output
-    //
-    new cdk.CfnOutput(this, 'S3Bucket', {
-      value: bucket.bucketName
-    });
-    new cdk.CfnOutput(this, 'PutURL', {
-      value: `https://${api.restApiId}.execute-api.${this.region}.amazonaws.com/prod/${bucket.bucketName}`
-    });
-  }
+        const s3proxy = api.root.addResource('{proxy+}',);
+        s3proxy.addMethod('GET', new apigateway.AwsIntegration({
+            service: 's3',
+            integrationHttpMethod: 'GET',
+            path: `${bucket.bucketName}/{objkey}`,
+            options: {
+                credentialsRole: role,
+                requestParameters: {
+                    'integration.request.path.objkey': 'method.request.path.proxy',
+                },
+                integrationResponses,
+            },
+        }), {
+            requestParameters: {
+                "method.request.path.proxy": true
+            }, methodResponses: methodResponses
+        });
+        s3proxy.addMethod('PUT', new apigateway.AwsIntegration({
+            service: 's3',
+            integrationHttpMethod: 'PUT',
+            path: `${bucket.bucketName}/{objkey}`,
+            options: {
+                credentialsRole: role,
+                requestParameters: {
+                    'integration.request.path.objkey': 'method.request.path.proxy',
+                },
+                integrationResponses,
+            },
+        }), {
+            requestParameters: {
+                "method.request.path.proxy": true
+            },
+            methodResponses: methodResponses
+        })
+
+        new cdk.CfnOutput(this, 'S3Bucket', {
+            value: bucket.bucketName
+        });
+        new cdk.CfnOutput(this, 'PutURL', {
+            value: `https://${api.restApiId}.execute-api.${this.region}.amazonaws.com/prod/${bucket.bucketName}`
+        });
+    }
 }
